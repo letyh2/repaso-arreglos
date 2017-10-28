f3070a84f4fa76fa115b6848d47826b9e8987e03
letyh2 <hatena898910@gmail.com>
2017-10-28 02:06:44 -0500

    codigo scii

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 2.- METODOS PARA LOS ESTRINGS
 3.- ASII
 4.- for,condiciones,(if,if else);
-![diagrama de flujo](diagrma de flujo.jpg)
+![diagrama de flujo](diagrma de flujo.jpg/C:\Users\kamus\proyecto-laboratoria\estructura-de-carpetas\fai-proyect-club)
--- js/app.js
+++ js/app.js
@@ -14,3 +14,15 @@ console.log(testArray);
 //revertir los elementod del arreglo
 testArray.reverse();
 console.log(testArray);
+/*---------------------------------------------------------------------------*/
+var testString = "abcde";
+testString.charAt(2);
+console.log(testString);
+// para obtener un caracter apartir de un codigo
+var codigo = testString.charCodeAt(2);
+console.log(codigo);
+
+//99
+String.fromCharCode(codigo);
+console.log(String.fromCharCode(codigo));
+//c
